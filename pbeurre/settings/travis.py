4e337d4f898ce2217bfb0c8981becc71290efa71
unknown <mfjotaa@hotmail.com>
2019-12-30 16:10:44 +0100

    env

--- pbeurre/settings/travis.py
+++ pbeurre/settings/travis.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': '',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': '1234',
         'HOST': '',
         'PORT': '',
     },

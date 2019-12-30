0a5ea19b18eda083f4b0bb53f62bf278be279207
unknown <mfjotaa@hotmail.com>
2019-12-30 17:05:40 +0100

    serv

--- pbeurre/wsgi.py
+++ pbeurre/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pbeurre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pbeurreserv.settings')
 
 application = get_wsgi_application()

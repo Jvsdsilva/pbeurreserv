f1106edb4c6cde4c7217c6783c0ff9fd2e35601f
Jvsdsilva <mfjotaa@hotmail.com>
2020-01-16 18:58:27 +0100

    static

--- pbeurre/settings/__init__.py
+++ pbeurre/settings/__init__.py
@@ -138,4 +138,4 @@ STATIC_URL = '/static/'
 # Django debug toolbar
 INTERNAL_IPS = ['127.0.0.1']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
